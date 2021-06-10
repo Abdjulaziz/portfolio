4d5d5e4630522b065d601572811b4a716dc482e2
Abdulaziz Al Otaibi <abdulazizpriv@gmail.com>
2021-06-10 22:43:07 +0200

    test

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="contact">
     <h2>Feel free to contact me <i class="fas fa-paper-plane"></i></h2>
-
     <a href="mailto: Abdulazizpriv@gmail.com"><p>Abdulazizpriv@gmail.com</p></a>
     <br />
-
     <p>Copyright 2021 Assao</p>
   </div>
 </template>
